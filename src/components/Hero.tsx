342742d31c09fd045219fae19a1a3e1f0fe655e2
THE-AryanKumar <lit2021030@iiitl.ac.in>
2023-12-23 01:24:33 +0530

    jai shree ram

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
       <div className="absolute top-0 w-full h-full bg-black/30 text-gray-100 flex flex-col items-center justify-center">
         <h2 className="text-7xl lg:text-[150px] font-bold">One Piece</h2>
         <p className="text-xl md:text-2xl lg:text-5xl font-semibold">
-          I'm gonna be king of the pirates
+          I&apos;m gonna be king of the pirates
         </p>
       </div>
     </div>
